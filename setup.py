0952be6b13304a94323e87fd38a4a5b130c9c166
Alex <77122480+Faddon@users.noreply.github.com>
2022-09-13 11:21:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='time-on-fire',
-    version='0.1.0',
-    author="zapChip",
+    version='0.1.1',
+    author="zapvolt",
     author_email="ag11012008@gmail.com",
     description="A simple activity tracker for Windows.",
+    license="GNU GPLv3",
     long_description_content_type="text/markdown",
     long_description=open("README.md").read(),
     packages=["time_on_fire"],
@@ -14,7 +15,7 @@ setup(
     },
     include_package_data=True,
     install_requires=[
-        "wheel", "click", "pywin32", "psutil", "tabulate"
+        "click", "pywin32", "psutil", "tabulate"
     ],
     keywords=["python time-tracking time tracking monitoring report"],
     classifiers=[
